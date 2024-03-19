8fbfe355d5fdbe7e476ec97118a877dec807cad9
NigelT97 <nigelmj97@gmail.com>
2024-03-19 19:35:28 +0200

    adding the first directory

---
+++ p_pract/msg1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+msg1 = "Welcome to programming python"
+print(msg1)
+
+msg1 = "This is the new message"
+print(msg1)
+
+name1 = "Nigel"
+print(f"welcome to pythom {name1}. Hope you will enjoy it")
+
+print(f"{msg1} {name1}".upper())
+
+# a new form of numbers
+add8, sub8, mul8, div8 = 2 + 6, 9 - 1, 2 * 4, 24 / 3
+print(f"results of addition, subtraction, division and multiplication\n {add8}\n {sub8}\n {mul8}\n {div8}")
+
+
